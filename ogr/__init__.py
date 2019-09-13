2a165b94b221f1ad59b806e09e3962dea332302a
Jan Ščotka <jscotka@redhat.com>
2019-09-13 18:26:57 +0200

    proper replacemenst

--- ogr/__init__.py
+++ ogr/__init__.py
@@ -44,12 +44,64 @@ except DistributionNotFound:
 
 mock_env = os.getenv("RECORD_REQUESTS")
 if mock_env:
+    from requre.import_system import upgrade_import_system, ReplaceType
+    from requre.helpers.requests_response import RequestResponseHandling
+
     MODULE_LIST = [
         ("^requests$", {"who_name": "ogr"}),
         ("^requests$", {"who_name": "gitlab"}),
         ("^requests$", {"who_name": "github"}),
+        (
+            "^requests$",
+            {"who_name": "ogr.services.pagure"},
+            {
+                "Session.request": [
+                    ReplaceType.DECORATOR,
+                    RequestResponseHandling.decorator(item_list=["method", "url"]),
+                ]
+            },
+        ),
+        (
+            "^requests$",
+            {"who_name": "gitlab"},
+            {
+                "Session.request": [
+                    ReplaceType.DECORATOR,
+                    RequestResponseHandling.decorator(item_list=["method", "url"]),
+                ]
+            },
+        ),
+        (
+            "^requests$",
+            {"who_name": "github.MainClass"},
+            {
+                "request": [
+                    ReplaceType.DECORATOR,
+                    RequestResponseHandling.decorator(item_list=["method", "url"]),
+                ]
+            },
+        ),
+        (
+            "^requests$",
+            {"who_name": "github.Requester"},
+            {
+                "Session.request": [
+                    ReplaceType.DECORATOR,
+                    RequestResponseHandling.decorator(item_list=["method", "url"]),
+                ]
+            },
+        ),
+        (
+            "^requests$",
+            {"who_name": "ogr.services.github_tweak"},
+            {
+                "request": [
+                    ReplaceType.DECORATOR,
+                    RequestResponseHandling.decorator(item_list=["method", "url"]),
+                ]
+            },
+        ),
     ]
-    from requre.import_system import upgrade_import_system
 
     upgrade_import_system(MODULE_LIST, debug_file="modules.out")
     from ogr.services.mock.github import (
