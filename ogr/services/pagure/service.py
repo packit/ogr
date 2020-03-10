39d1714056385a305b81d1e4c21985c76a66773d
Dominika Hodovska <dhodovsk@redhat.com>
2020-03-10 14:00:09 +0100

    stage deployment changes

--- ogr/services/pagure/service.py
+++ ogr/services/pagure/service.py
@@ -40,6 +40,7 @@ logger = logging.getLogger(__name__)
 @use_for_service("src.fedoraproject.org")
 @use_for_service("git.stg.centos.org")
 @use_for_service("git.centos.org")
+@use_for_service("src.stg.fedoraproject.org")
 class PagureService(BaseGitService):
     def __init__(
         self,
@@ -134,7 +135,7 @@ class PagureService(BaseGitService):
                 else None
             )
             raise PagureAPIException(
-                f"Page '{url}' not found when calling Pagure API.",
+                f"Page `{url}` not found when calling Pagure API.",
                 pagure_error=error_msg,
             )
 
@@ -146,14 +147,14 @@ class PagureService(BaseGitService):
             logger.error(response.json_content)
             if "error" in response.json_content:
                 error_msg = response.json_content["error"]
-                error_msg_ext = response.json_content.get("errors", "")
-                msg = f"Pagure API returned an error when calling '{url}': {error_msg}"
-                if error_msg_ext:
-                    msg += f" - {error_msg_ext}"
+                error_msg_ext = response.json_content["errors"]
                 raise PagureAPIException(
-                    msg, pagure_error=error_msg, pagure_response=response.json_content,
+                    f"Pagure API returned an error when calling `{url}`: "
+                    f"{error_msg} - {error_msg_ext}",
+                    pagure_error=error_msg,
+                    pagure_response=response.json_content,
                 )
-            raise PagureAPIException(f"Problem with Pagure API when calling '{url}'")
+            raise PagureAPIException(f"Problem with Pagure API when calling `{url}`")
 
         return response.json_content
 
@@ -168,7 +169,7 @@ class PagureService(BaseGitService):
 
         except requests.exceptions.ConnectionError as er:
             logger.error(er)
-            raise PagureAPIException(f"Cannot connect to url: '{url}'.", er)
+            raise PagureAPIException(f"Cannot connect to url: `{url}`.", er)
         return response
 
     def get_raw_request(
