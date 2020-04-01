0b3f88dc14e6fedd2bf74a5bbd87d393f2acc00c
Jan Sakalos <sakalosj@gmail.com>
2020-04-01 15:52:53 +0200

    reduced initial data_pagure_raw_pr configuration to follow YAGNI principle and reduce code

--- tests/unit/conftest.py
+++ tests/unit/conftest.py
@@ -1,6 +1,8 @@
 import pytest
 from flexmock import flexmock
 
+from ogr.services.pagure import PagureProject
+
 
 @pytest.fixture
 def mock_pull_request():
@@ -21,6 +23,12 @@ def mock_github_repo(mock_pull_request):
 
 
 @pytest.fixture
+def pagure_project(mock_github_repo):
+    mock = flexmock(spec=PagureProject)
+    return mock
+
+
+@pytest.fixture
 def mock_pagure_repo(mock_pull_request):
     def mock_pagure_repo_factory():
         mock = flexmock(create_pull=mock_pull_request(42))
@@ -31,145 +39,15 @@ def mock_pagure_repo(mock_pull_request):
 
 @pytest.fixture
 def data_pagure_raw_pr():
-    def data_pagure_raw_pr_factory(commit_stop: int = 123456):
+    """
+    Fixtures generates PagurePullRequest._raw_pr value(s).
+    :return: _raw_pr factory function
+
+    """
+
+    def data_pagure_raw_pr_factory(commit_stop: str = "123456abcd"):
         return {
-            "assignee": None,
-            "branch": "master",
-            "branch_from": "master",
-            "cached_merge_status": "FFORWARD",
-            "closed_at": None,
-            "closed_by": None,
-            "comments": [
-                {
-                    "comment": "comment1",
-                    "commit": None,
-                    "date_created": "1584877336",
-                    "edited_on": None,
-                    "editor": None,
-                    "filename": None,
-                    "id": 149,
-                    "line": None,
-                    "notification": False,
-                    "parent": None,
-                    "reactions": {},
-                    "tree": None,
-                    "user": {
-                        "fullname": "J\u00e1n Sak\u00e1lo\u0161",
-                        "name": "sakalosj",
-                    },
-                },
-                {
-                    "comment": "comment2",
-                    "commit": None,
-                    "date_created": "1584879440",
-                    "edited_on": None,
-                    "editor": None,
-                    "filename": None,
-                    "id": 150,
-                    "line": None,
-                    "notification": False,
-                    "parent": None,
-                    "reactions": {},
-                    "tree": None,
-                    "user": {
-                        "fullname": "J\u00e1n Sak\u00e1lo\u0161",
-                        "name": "sakalosj",
-                    },
-                },
-            ],
-            "commit_start": "aca83b681cabf99061843595203ca1330d866de4",
             "commit_stop": commit_stop,
-            "date_created": "1584815643",
-            "id": 9,
-            "initial_comment": "pr body",
-            "last_updated": "1585230213",
-            "project": {
-                "access_groups": {
-                    "admin": ["git-packit-team"],
-                    "commit": [],
-                    "ticket": [],
-                },
-                "access_users": {
-                    "admin": ["sakalosj"],
-                    "commit": [],
-                    "owner": ["packit"],
-                    "ticket": [],
-                },
-                "close_status": [],
-                "custom_keys": [],
-                "date_created": "1584797507",
-                "date_modified": "1585301472",
-                "description": "packit test repo",
-                "fullname": "source-git/packit-hello-world",
-                "id": 6843,
-                "milestones": {},
-                "name": "packit-hello-world",
-                "namespace": "source-git",
-                "parent": None,
-                "priorities": {},
-                "tags": [],
-                "url_path": "source-git/packit-hello-world",
-                "user": {"fullname": "Packit Team", "name": "packit"},
-            },
-            "remote_git": None,
-            "repo_from": {
-                "access_groups": {"admin": [], "commit": [], "ticket": []},
-                "access_users": {
-                    "admin": [],
-                    "commit": [],
-                    "owner": ["sakalosj"],
-                    "ticket": [],
-                },
-                "close_status": [],
-                "custom_keys": [],
-                "date_created": "1584799939",
-                "date_modified": "1584799939",
-                "description": "packit test repo",
-                "fullname": "forks/sakalosj/source-git/packit-hello-world",
-                "id": 6845,
-                "milestones": {},
-                "name": "packit-hello-world",
-                "namespace": "source-git",
-                "parent": {
-                    "access_groups": {
-                        "admin": ["git-packit-team"],
-                        "commit": [],
-                        "ticket": [],
-                    },
-                    "access_users": {
-                        "admin": ["sakalosj"],
-                        "commit": [],
-                        "owner": ["packit"],
-                        "ticket": [],
-                    },
-                    "close_status": [],
-                    "custom_keys": [],
-                    "date_created": "1584797507",
-                    "date_modified": "1585301472",
-                    "description": "packit test repo",
-                    "fullname": "source-git/packit-hello-world",
-                    "id": 6843,
-                    "milestones": {},
-                    "name": "packit-hello-world",
-                    "namespace": "source-git",
-                    "parent": None,
-                    "priorities": {},
-                    "tags": [],
-                    "url_path": "source-git/packit-hello-world",
-                    "user": {"fullname": "Packit Team", "name": "packit"},
-                },
-                "priorities": {},
-                "tags": [],
-                "url_path": "fork/sakalosj/source-git/packit-hello-world",
-                "user": {"fullname": "J\u00e1n Sak\u00e1lo\u0161", "name": "sakalosj"},
-            },
-            "status": "Open",
-            "tags": [],
-            "threshold_reached": None,
-            "title": "pr title",
-            "uid": "d54ec33f2ba54730905d3494c677a767",
-            "updated_on": "1585230213",
-            "user": {"fullname": "J\u00e1n Sak\u00e1lo\u0161", "name": "sakalosj"},
         }
 
     return data_pagure_raw_pr_factory
--- tests/unit/test_pagure.py
+++ tests/unit/test_pagure.py
@@ -1,33 +1,10 @@
-import pytest
-from flexmock import flexmock
-
-from ogr.services.pagure import PagureProject, PagurePullRequest
-
-
-@pytest.fixture
-def pagure_project(mock_github_repo):
-    pagure_project = PagureProject(
-        repo="test_repo", service="test_service", namespace="test_namespace"
-    )
-    parent_pagure_project = PagureProject(
-        repo="test_parent_repo",
-        service="test_service",
-        namespace="test_parent_namespace",
-    )
-    flexmock(pagure_project)
-    flexmock(parent_pagure_project)
-    flexmock(PagurePullRequest)
-
-    # pagure_project.should_receive("pagure_repo").and_return(mock_pagure_repo())
-    # parent_pagure_project.should_receive("pagure_repo").and_return(mock_pagure_repo())
-    # pagure_project.should_receive("parent").and_return(parent_pagure_project)
-    return pagure_project
+from ogr.services.pagure import PagurePullRequest
 
 
 class TestPullRequest:
-    def test_latest_commit(self, data_pagure_raw_pr):
+    def test_latest_commit(self, data_pagure_raw_pr, pagure_project):
         pagure_pullreqest = PagurePullRequest(
-            raw_pr=data_pagure_raw_pr(commit_stop="1a2b3c"), project=None
+            raw_pr=data_pagure_raw_pr(commit_stop="1a2b3c"), project=pagure_project
         )
 
         assert pagure_pullreqest.head_commit == "1a2b3c"
