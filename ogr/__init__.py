8dd346b846d20804209d20d0ab42cff228da40db
abkosar <berkaykosar@sabanciuniv.edu>
2021-10-10 23:18:50 -0400

    pdoc implementation

--- ogr/__init__.py
+++ ogr/__init__.py
@@ -13,9 +13,10 @@ from ogr.factory import (
     get_service_class_or_none,
     get_instances_from_dict,
 )
-from ogr.services.github import GithubService
-from ogr.services.gitlab import GitlabService
-from ogr.services.pagure import PagureService
+
+from ogr.services.github import *
+from ogr.services.gitlab import *
+from ogr.services.pagure import *
 
 try:
     __version__ = get_distribution(__name__).version
@@ -24,11 +25,39 @@ except DistributionNotFound:
     pass
 
 __all__ = [
+    #services.github
     GithubService.__name__,
-    PagureService.__name__,
+    GithubCheckRun.__name__,
+    GithubPullRequest.__name__,
+    GithubIssueComment.__name__,
+    GithubPRComment.__name__,
+    GithubIssue.__name__,
+    GithubRelease.__name__,
+    GithubUser.__name__,
+    GithubProject.__name__,
+
+    #services.gitlab
     GitlabService.__name__,
+    GitlabIssue.__name__,
+    GitlabPullRequest.__name__,
+    GitlabIssueComment.__name__,
+    GitlabPRComment.__name__,
+    GitlabRelease.__name__,
+    GitlabUser.__name__,
+    GitlabProject.__name__,
+
+    PagureService.__name__,
+    PagurePullRequest.__name__,
+    PagureIssueComment.__name__,
+    PagurePRComment.__name__,
+    PagureIssue.__name__,
+    PagureRelease.__name__,
+    PagureUser.__name__,
+    PagureProject.__name__,
+
     get_project.__name__,
     get_service_class.__name__,
     get_service_class_or_none.__name__,
     get_instances_from_dict.__name__,
 ]
+
