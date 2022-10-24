164a803ed35a30b68b94927525b5931eec29e690
František Nečas <fnecas@redhat.com>
2022-10-24 08:04:10 +0200

    Remove old deprecations
    Signed-off-by: František Nečas <fnecas@redhat.com>

--- ogr/exceptions.py
+++ ogr/exceptions.py
@@ -6,8 +6,6 @@ from typing import Optional, Dict, Any
 import github
 import gitlab
 
-from ogr.deprecation import deprecate_and_set_removal
-
 
 class OgrException(Exception):
     """Something went wrong during our execution."""
@@ -50,15 +48,6 @@ class PagureAPIException(APIException):
 class GithubAPIException(APIException):
     """Exception related to Github API."""
 
-    @property  # type: ignore
-    @deprecate_and_set_removal(
-        since="0.30.0",
-        remove_in="0.35.0 (or 1.0.0 if it comes sooner)",
-        message="Use __cause__",
-    )
-    def github_error(self):
-        return self.__cause__
-
     @property
     def response_code(self):
         if self.__cause__ is None or not isinstance(
@@ -70,15 +59,6 @@ class GithubAPIException(APIException):
 
 class GitlabAPIException(APIException):
     """Exception related to Gitlab API."""
-
-    @property  # type: ignore
-    @deprecate_and_set_removal(
-        since="0.30.0",
-        remove_in="0.35.0 (or 1.0.0 if it comes sooner)",
-        message="Use __cause__",
-    )
-    def gitlab_error(self):
-        return self.__cause__
 
     @property
     def response_code(self):
