2f92fe46c90ed642dae7fb94f93092134257926c
Matej Focko <mfocko@redhat.com>
2025-03-10 09:58:48 +0100

    chore(forgejo): add stubs
    Signed-off-by: Matej Focko <mfocko@redhat.com>

--- ogr/services/forgejo/issue.py
+++ ogr/services/forgejo/issue.py
@@ -1,10 +1,40 @@
 # Copyright Contributors to the Packit project.
 # SPDX-License-Identifier: MIT
 
+from typing import Optional
+
+from pyforgejo.types import Issue as PyforgejoIssue
+
+from ogr.abstract import Issue, IssueStatus
 from ogr.services import forgejo
 from ogr.services.base import BaseIssue
 
 
 class ForgejoIssue(BaseIssue):
-    def __init__(self, raw_issue, project: "forgejo.ForgejoProject"):
+    def __init__(self, raw_issue: PyforgejoIssue, project: "forgejo.ForgejoProject"):
         super().__init__(raw_issue, project)
+
+    @staticmethod
+    def create(
+        project: "forgejo.ForgejoProject",
+        title: str,
+        body: str,
+        private: Optional[bool] = None,
+        labels: Optional[list[str]] = None,
+        assignees: Optional[list] = None,
+    ) -> "Issue":
+        raise NotImplementedError("TBD")
+
+    @staticmethod
+    def get(project: "forgejo.ForgejoProject", issue_id: int) -> "Issue":
+        raise NotImplementedError("TBD")
+
+    @staticmethod
+    def get_list(
+        project: "forgejo.ForgejoProject",
+        status: IssueStatus = IssueStatus.open,
+        author: Optional[str] = None,
+        assignee: Optional[str] = None,
+        labels: Optional[list[str]] = None,
+    ) -> list["Issue"]:
+        raise NotImplementedError("TBD")
