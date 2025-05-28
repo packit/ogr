e3530454d8874f5b05ab8fe7efeae91aaef65b80
Cristian Le <git@lecris.dev>
2025-05-28 10:54:51 +0200

    Implement `GithubCommit.get_prs`
    Signed-off-by: Cristian Le <git@lecris.dev>

--- ogr/services/github/commit.py
+++ ogr/services/github/commit.py
@@ -1,12 +1,16 @@
 # Copyright Contributors to the Packit project.
 # SPDX-License-Identifier: MIT
+from typing import Optional
+
 import github
 from github.Commit import Commit as _GitCommit
 
+from ogr.abstract import PullRequest
 from ogr.exceptions import GithubAPIException
 from ogr.services import github as ogr_github
 from ogr.services.base import BaseGitCommit
 from ogr.services.github.changes import GithubCommitChanges
+from ogr.services.github.pull_request import GithubPullRequest
 
 
 class GithubCommit(BaseGitCommit):
@@ -22,6 +26,12 @@ class GithubCommit(BaseGitCommit):
         if not self._changes:
             self._changes = GithubCommitChanges(self)
         return self._changes
+
+    def get_prs(self) -> Optional[list[PullRequest]]:
+        raw_prs = list(self._raw_commit.get_pulls())
+        if not raw_prs:
+            return None
+        return [GithubPullRequest(pr, self.project) for pr in raw_prs]
 
     @staticmethod
     def get(project: "ogr_github.GithubProject", sha: str) -> "GithubCommit":
